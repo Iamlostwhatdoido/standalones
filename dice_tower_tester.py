66917acf09ad1cda9b168a7ce72fd48019c3137f
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-14 11:20:44 +0100

    Added description header

--- dice_tower_tester.py
+++ dice_tower_tester.py
@@ -1,3 +1,15 @@
+"""
+Jolan ARNAUD
+Executable with input/output in terminal
+Tests the repartition of roll result through a dice tower.
+First roll is made with face 1 upward.
+Result is inputted, subsequent rolls are made with previous result face up.
+Final matrix shows rolls repartition.
+Ideally all matrix cells should be the same.
+
+"""
+
+
 import numpy as np
 
 dice_size = int(input("Dice size : "))
